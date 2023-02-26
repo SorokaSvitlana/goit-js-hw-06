f94960b422bcdffc8cf19a138fd1b0b817c20479
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-02-26 16:59:44 +0200

    done 9 task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector(".login-form");
+const inputs = document.querySelectorAll('input');
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formEls = event.currentTarget.elements;
+
+  const mail = formEls.mail;
+  const password = formEls.password;
+
+  let hasError = false;
+
+  inputs.forEach(input => {
+    if (input.value.trim() === '') {
+      alert("Please fill in all fields.");
+      hasError = true;
+    }
+  });
+
+  if (!hasError) {
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+      console.log(name, value);
+    });
+    event.currentTarget.reset();
+  }
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,18 @@
+const body = document.querySelector('body')
+const btn = document.querySelector('.change-color');
+const colorText = document.querySelector('.color')
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+btn.addEventListener('click', handleSubmit)
+
+function handleSubmit(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  colorText.textContent = getRandomHexColor();
 }
